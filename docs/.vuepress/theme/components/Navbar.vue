2f20e5e56114d0ac56bbfdd88db4057b9560fbd8
Favo Yang <favoyang@gmail.com>
2019-09-21 21:40:12 +0800

    Added under-development flag.

---
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ParentLayout></ParentLayout>
+    <div class="wip label label-secondary">under development</div>
+  </div>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/components/Navbar.vue'
+
+export default {
+  components: {
+    ParentLayout,
+  }
+}
+</script>
+
+<style lang="stylus">
+  .wip
+    position absolute
+    z-index 1000
+    left 200px
+    top 8px
+</style>
