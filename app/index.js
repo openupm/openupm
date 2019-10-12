fc60f3f1a33cfd9e10db20f9f596f82cf2502fca
Favo Yang <favoyang@gmail.com>
2019-10-12 11:57:35 +0800

    Added extra headers for netlify.

--- app/index.js
+++ app/index.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Expose-Headers", "Content-Length");
   res.header(
     "Access-Control-Allow-Headers",
-    "Accept, Authorization, Content-Type, X-Requested-With, Range, Origin"
+    "Accept,Authorization,Content-Type,X-Requested-With,Range,Origin"
   );
   if (req.method === "OPTIONS") {
     return res.send(200);
