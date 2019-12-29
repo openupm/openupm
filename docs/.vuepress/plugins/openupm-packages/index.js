799872964c696c18f3f03889dbb96463c7b9a7fe
Favo Yang <favoyang@gmail.com>
2019-12-29 19:47:21 +0800

    refactor: split openupm-packages to small functions

--- docs/.vuepress/plugins/openupm-packages/index.js
+++ docs/.vuepress/plugins/openupm-packages/index.js
@@ -8,109 +8,148 @@ const {
 } = require("../../../../app/utils/package");
 
 // eslint-disable-next-line no-unused-vars
-module.exports = (options, context) => ({
-  name: "openupm-packages",
+module.exports = function(options, context) {
+  let plugin = {
+    /*** Plugin interface ****/
+    name: "openupm-packages",
 
-  async extendPageData($page) {
-    let packageNames = await loadPackageNames();
-    $page.packageCount = packageNames.length;
-  },
+    async extendPageData($page) {
+      let packageNames = await loadPackageNames();
+      $page.packageCount = packageNames.length;
+    },
 
-  async additionalPages() {
-    // Load packages.
-    let packageNames = await loadPackageNames();
-    let packages = packageNames
-      .map(loadPackageSync)
-      .filter(x => x)
-      .map(pkg => {
-        return {
-          ...pkg,
-          link: {
-            link: `/packages/${pkg.name}/`,
-            text: pkg.displayName || pkg.name
-          }
+    async additionalPages() {
+      const data = await plugin.getData();
+      let pages = [];
+      pages = pages.concat(await plugin.genListPages(data));
+      pages = pages.concat(await plugin.genDetailPages(data));
+      pages = pages.concat(await plugin.genAddPage(data));
+      return pages;
+    },
+
+    /*** Page generators ****/
+
+    // Prepare data for page generation
+    async getData() {
+      // Load packages.
+      let packageNames = await loadPackageNames();
+      let packages = packageNames
+        .map(loadPackageSync)
+        .filter(x => x)
+        .map(pkg => {
+          return {
+            ...pkg,
+            link: {
+              link: `/packages/${pkg.name}/`,
+              text: pkg.displayName || pkg.name
+            }
+          };
+        })
+        // Sort by name
+        .sort(function(a, b) {
+          let va = a.link.text.toLowerCase();
+          let vb = b.link.text.toLowerCase();
+          if (va < vb) return -1;
+          if (va > vb) return 1;
+          return 0;
+        });
+      // Load topics.
+      let topicsWithAll = [{ name: "All", slug: "" }]
+        .concat((await loadTopics()).topics)
+        .map(topic => {
+          return {
+            ...topic,
+            link: topic.slug ? `/packages/topics/${topic.slug}/` : "/packages/",
+            count: packages.filter(pkg =>
+              plugin.packageTopicFilter(pkg, topic.slug)
+            ).length
+          };
+        });
+      let topics = topicsWithAll.slice(1);
+      return { packageNames, packages, topics, topicsWithAll };
+    },
+
+    // Generate package list pages
+    async genListPages(data) {
+      let pages = [];
+      for (let topic of data.topicsWithAll) {
+        // Skip topic with no packages.
+        if (topic.slug && topic.count == 0) continue;
+        let frontmatter = {
+          layout: "PackageList",
+          noGlobalSocialShare: true,
+          title: topic.slug ? `Packages - ${topic.name}` : "Packages",
+          topics: data.topicsWithAll,
+          topic,
+          packages: data.packages.filter(pkg =>
+            plugin.packageTopicFilter(pkg, topic.slug)
+          )
         };
-      })
-      // Sort by name
-      .sort(function(a, b) {
-        let va = a.link.text.toLowerCase();
-        let vb = b.link.text.toLowerCase();
-        if (va < vb) return -1;
-        if (va > vb) return 1;
-        return 0;
+        pages.push({
+          path: topic.link,
+          content: plugin.createPage(frontmatter)
+        });
+      }
+      return pages;
+    },
+
+    // Generate package detail page
+    async genDetailPages(data) {
+      let pages = [];
+      for (let pkg of data.packages) {
+        let frontmatter = {
+          layout: "PackageDetail",
+          title: pkg.displayName
+            ? `📦 ${pkg.displayName} - ${pkg.name}`
+            : `📦 ${pkg.name}`,
+          package: pkg
+        };
+        pages.push({
+          path: "/packages/" + pkg.name + "/",
+          content: plugin.createPage(frontmatter)
+        });
+      }
+      return pages;
+    },
+
+    // Generate package detail page
+    async genAddPage(data) {
+      let pages = [];
+      pages.push({
+        path: "/packages/add/",
+        content: plugin.createPage({
+          layout: "PackageAdd",
+          title: "Package Add",
+          licenses: Object.keys(spdx)
+            .sort(function(a, b) {
+              return spdx[a].name
+                .toLowerCase()
+                .localeCompare(spdx[b].name.toLowerCase());
+            })
+            .map(function(key) {
+              return { id: key, name: spdx[key].name };
+            }),
+          packageNames: data.packageNames,
+          topics: data.topics
+        })
       });
-    // Package topic filter function.
-    let packageTopicFilter = (pkg, topicSlug) => {
+      return pages;
+    },
+
+    /*** Helpers ****/
+
+    // Package topic filter function
+    packageTopicFilter(pkg, topicSlug) {
       if (pkg.excludedFromList) return false;
       else if (!topicSlug) return true;
       else if (Array.isArray(pkg.topics)) return pkg.topics.includes(topicSlug);
       else return pkg.topics == topicSlug;
-    };
-    // Load topics.
-    let topicsWithAll = [{ name: "All", slug: "" }]
-      .concat((await loadTopics()).topics)
-      .map(topic => {
-        return {
-          ...topic,
-          link: topic.slug ? `/packages/topics/${topic.slug}/` : "/packages/",
-          count: packages.filter(pkg => packageTopicFilter(pkg, topic.slug))
-            .length
-        };
-      });
-    let topics = topicsWithAll.slice(1);
-    // Pages
-    let pages = [];
-    let createPageContent = fm => "---\n" + yaml.safeDump(fm) + "\n---\n";
-    // List pages.
-    for (let topic of topicsWithAll) {
-      // Skip topic with no packages.
-      if (topic.slug && topic.count == 0) continue;
-      let frontmatter = {
-        layout: "PackageList",
-        noGlobalSocialShare: true,
-        title: topic.slug ? `Packages - ${topic.name}` : "Packages",
-        topics: topicsWithAll,
-        topic,
-        packages: packages.filter(pkg => packageTopicFilter(pkg, topic.slug))
-      };
-      pages.push({
-        path: topic.link,
-        content: createPageContent(frontmatter)
-      });
+    },
+
+    // Create page content from frontmatter
+    createPage(frontmatter) {
+      return "---\n" + yaml.safeDump(frontmatter) + "\n---\n";
     }
-    // Detail pages.
-    for (let pkg of packages) {
-      let frontmatter = {
-        layout: "PackageDetail",
-        title: pkg.displayName
-          ? `📦 ${pkg.displayName} - ${pkg.name}`
-          : `📦 ${pkg.name}`,
-        package: pkg
-      };
-      pages.push({
-        path: "/packages/" + pkg.name + "/",
-        content: createPageContent(frontmatter)
-      });
-    }
-    // Add pages.
-    pages.push({
-      path: "/packages/add/",
-      content: createPageContent({
-        layout: "PackageAdd",
-        title: "Package Add",
-        licenses: Object.keys(spdx)
-          .sort(function(a, b) {
-            return spdx[a].name
-              .toLowerCase()
-              .localeCompare(spdx[b].name.toLowerCase());
-          })
-          .map(function(key) {
-            return { id: key, name: spdx[key].name };
-          }),
-        packageNames,
-        topics
-      })
-    });
-    return pages;
-  }
-});
+  };
+  return plugin;
+};
