ba8cbc8a552c5d47584c1b36e22815f33d0d557c
Favo Yang <favoyang@gmail.com>
2023-11-02 00:17:05 +0800

    chore: clean up eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,11 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/recommended",
-    "prettier/vue",
-    "plugin:prettier/recommended"
-  ],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
   rules: {
-    "vue/component-name-in-template-casing": ["error", "PascalCase"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  }
-}
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+};
